816128e16d5e8a4e0c4722e22fc5240b07eb1038
khalil951 <mohamedkhalil.ouelsleti@esprit.tn>
2023-06-22 01:01:19 +0100

    modifyed pw

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '8793fb69e50e9558a12a8aa6b548d52880f2290b'
-app.config["MONGO_URI"] = "mongodb+srv://khalil:YchPOJXzKBjMPOZI@cluster0.0azwkml.mongodb.net/myDB?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://khalil:<password>@cluster0.0azwkml.mongodb.net/myDB?retryWrites=true&w=majority"
 #setup mongodb
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
